2b362bac3385c4bdd88c4c929e8cf8da4d768ebe
TheCodingJungle <codingjungle1729@gmail.com>
2021-09-09 23:30:25 +0530

    Adding Directory Tree project

---
+++ DirectoryTree/DirectoryTree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryTree
+{
+    class Program
+    {
+        private static readonly string depthString = "--";
+
+        /**
+         * @breif Recursive helper for the printDirectoryTree.
+         * @param path: path to print.
+         * @param depth: level of sub folders.
+      
+         */
+        private static void directoryTreeHelper(string path, int depth)
+        {
+            string[] contentFiles = Directory.GetFiles(path);
+            string[] contentDirectory = Directory.GetDirectories(path);
+            int i;
+
+            // for is for the int i = 0 to n type of loops.
+            foreach(string file in contentFiles)
+            {
+                for (i = 0; i < depth; i++)
+                {
+                    Console.Write(depthString);
+                }
+                Console.WriteLine(file);
+            }
+
+            foreach(string dir in contentDirectory)
+            {
+                for (i = 0; i < depth; i++)
+                {
+                    Console.Write(depthString);
+                }
+                Console.WriteLine(dir);
+                directoryTreeHelper(dir, depth + 1);
+            }
+        }
+
+        /**
+         * @breif A function to print the directory tree for given path.
+         * @param path: Path to print.
+         */
+        static void printDirectoryTree(string path)
+        {
+            directoryTreeHelper(path, 0);
+        }
+
+        static void Main(string[] args)
+        {
+            printDirectoryTree(".");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,10 +3,16 @@ Learning C# and making some projects in it.
 
 ## Projects Implemented
 
-- TicTacToeConsoleApp
-    - The classic Tic-Tac-Toe game console application
+- Console Apps
+    - TicTacToeConsoleApp
+        - The classic Tic-Tac-Toe game console application
+    - Directory-Tree
+        - Lists down files and directories and listing in tree view with recursion
 
 ## Currently Working On
 
-- Directory-Tree
-    - Lists down files and directories and listing in tree view with recursion
+- Console Apps
+    - Calculator App
+        - Console based calculator app that helps evaluate a string expression
+    - Folder Sorter App
+        - Sorts the files based on there extension
