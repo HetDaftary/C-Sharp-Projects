ad6149f0b6cf44bb57ce9986a635c6828ee6b1a0
TheCodingJungle <codingjungle1729@gmail.com>
2021-09-09 12:51:13 +0530

    minor update

--- TicTacToeConsoleApp/TicTacToeConsoleApp/Program.cs
+++ TicTacToeConsoleApp/TicTacToeConsoleApp/Program.cs
@@ -238,6 +238,7 @@ namespace TicTacToeConsoleApp
                 Console.WriteLine("Enter q to quit");
                 // Providing user with description.
 
+                // You can also use str.toLower here but I do not want to use that.
                 option = makeLower(Console.ReadLine()[0]);
                 // Reading a character from the console and converting it to lower case.
                 
